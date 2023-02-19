5b0e962efd3c43942db0dca437b53fc0dbc4f641
dhrumillimbad <dhrumillimbad@gmail.com>
2023-02-19 17:57:15 -0500

    installed jest & react-test-renderer

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -1,14 +1,20 @@
-/**
- * @format
- */
+// /**
+//  * @format
+//  */
 
-import 'react-native';
-import React from 'react';
-import App from '../App';
+// import 'react-native';
+// import React from 'react';
+// import App from '../App';
 
-// Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
+// // Note: test renderer must be required after react-native.
+// import renderer from 'react-test-renderer';
 
-it('renders correctly', () => {
-  renderer.create(<App />);
+// it('renders correctly', () => {
+//   renderer.create(<App />);
+// });
+
+const sum = require('../utils/sum');
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3);
 });
