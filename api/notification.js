159abe6d42d373dac5d522cbf736c8d03b2cd93b
praj31 <rajputpraj.31@gmail.com>
2023-02-20 16:40:49 -0500

    bug resolved; conflict with alarm interval of previous reminder

--- api/notification.js
+++ api/notification.js
@@ -16,7 +16,8 @@ export const schedulePushNotification = async (title, date) => {
     channel: 'my_channel_id',
     small_icon: 'ic_launcher',
     has_button: true,
-    data: {message: title || 'test'},
+    data: { message: title },
+    volume: 1.0
   };
   let alDate = ReactNativeAN.parseDate(trigger);
   const setAlarmData = await ReactNativeAN.scheduleAlarm({
