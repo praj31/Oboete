6a319b61231f1d75e7301d63d078b6189b4e0666
Bhavik Gevariya <bgevariya20@gmail.com>
2023-02-21 20:57:48 -0500

    edit reminder

--- screens/ListReminder.js
+++ screens/ListReminder.js
@@ -1,14 +1,14 @@
-import React, { useEffect, useState } from 'react'
-import { View, Text, StyleSheet, TouchableOpacity, } from 'react-native'
+import React, {useEffect, useState} from 'react';
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
-import { getData, removeKey } from '../api/storage';
-import { deleteAlarms } from '../api/alarm';
+import {getData, removeKey} from '../api/storage';
+import {deleteAlarms} from '../api/alarm';
 import moment from 'moment';
 
-const ListReminder = (props) => {
+const ListReminder = props => {
   const [reminder, setReminder] = useState({});
   const navigation = props.navigation;
-  const id = props.route.params.id
+  const id = props.route.params.id;
 
   React.useEffect(() => {
     async function fetchData() {
@@ -16,23 +16,25 @@ const ListReminder = (props) => {
       if (reminder) {
         setReminder(reminder);
       } else {
-        alert("Error in fetching the event data")
-        navigation.goBack()
+        alert('Error in fetching the event data');
+        navigation.goBack();
       }
     }
     fetchData();
-  }, [])
+  }, []);
 
   const deleteEvent = async id => {
     await deleteAlarms(id);
     await removeKey(id);
-    navigation.navigate("Home")
+    navigation.navigate('Home');
   };
 
   return (
     <View style={styles.container}>
       <View style={styles.header}>
-        <TouchableOpacity style={styles.backBtn} onPress={() => navigation.goBack()} >
+        <TouchableOpacity
+          style={styles.backBtn}
+          onPress={() => navigation.goBack()}>
           <Icon name="arrow-back" size={30} color="#111" />
         </TouchableOpacity>
         <Text style={styles.h1}>Reminder Details</Text>
@@ -42,9 +44,13 @@ const ListReminder = (props) => {
           <Text style={styles.title}>Title</Text>
           <Text style={styles.value}>{reminder.title}</Text>
           <Text style={styles.title}>Date</Text>
-          <Text style={styles.value}>{moment(reminder.datetime, "YYYY-MM-DD LT").format("LL")}</Text>
+          <Text style={styles.value}>
+            {moment(reminder.datetime, 'YYYY-MM-DD LT').format('LL')}
+          </Text>
           <Text style={styles.title}>Time</Text>
-          <Text style={styles.value}>{moment(reminder.datetime, "YYYY-MM-DD LT").format("LT")}</Text>
+          <Text style={styles.value}>
+            {moment(reminder.datetime, 'YYYY-MM-DD LT').format('LT')}
+          </Text>
           <Text style={styles.title}>Interval</Text>
           <Text style={styles.value}>{reminder.interval} minute(s)</Text>
           <Text style={styles.title}>Repeat</Text>
@@ -55,22 +61,23 @@ const ListReminder = (props) => {
         <View style={styles.buttonContainer}>
           <TouchableOpacity
             style={[styles.actionBtn, styles.deleteBtn]}
-            onPress={() => { deleteEvent(id) }}
-          >
-            <Text style={{ color: '#fff', fontSize: 16 }}>Delete</Text>
+            onPress={() => {
+              deleteEvent(id);
+            }}>
+            <Text style={{color: '#fff', fontSize: 16}}>Delete</Text>
           </TouchableOpacity>
         </View>
         <View style={styles.buttonContainer}>
           <TouchableOpacity
             style={[styles.actionBtn, styles.editBtn]}
-          >
-            <Text style={{ fontSize: 16, color: '#111' }}>Edit</Text>
+            onPress={() => navigation.navigate('EditReminder', {id: id})}>
+            <Text style={{fontSize: 16, color: '#111'}}>Edit</Text>
           </TouchableOpacity>
         </View>
       </View>
     </View>
-  )
-}
+  );
+};
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -79,14 +86,14 @@ const styles = StyleSheet.create({
     padding: 24,
   },
   header: {
-    flexDirection: "row",
-    alignItems: "center",
-    marginBottom: 16
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 16,
   },
   h1: {
     fontSize: 32,
     fontWeight: 'bold',
-    color: "#111",
+    color: '#111',
   },
   backBtn: {
     marginRight: 8,
@@ -98,7 +105,7 @@ const styles = StyleSheet.create({
   value: {
     fontSize: 18,
     color: '#111',
-    marginBottom: 12
+    marginBottom: 12,
   },
   footer: {
     position: 'absolute',
@@ -126,5 +133,5 @@ const styles = StyleSheet.create({
     borderColor: '#333',
     backgroundColor: 'white',
   },
-})
-export default ListReminder
+});
+export default ListReminder;
